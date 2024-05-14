72838de1f66c5cc4d4f69fe49556a2137459e91b
allansdefreitas <allan.allan.santos@gmail.com>
2024-05-14 12:01:05 -0300

    JS

--- JavaScript/1- aluramidi-curso/main.js
+++ JavaScript/1- aluramidi-curso/main.js
@@ -35,19 +35,11 @@ function playSongTom(){
 }
 
 
-// Add a value to a property. Store a function and not run it (to run, use the function name with '()' )
-document.querySelector('.tecla_pom').onclick = playSongPom;
-document.querySelector('.tecla_clap').onclick = playSongClap;
+const listOfKeys = document.querySelectorAll('.tecla');
 
-/* Another way */
-const timButton = document.querySelector('.tecla_tim');
-timButton.addEventListener('click', playSongTim);
 
-document.querySelector('.tecla_puff').onclick = playSongPuff;
-document.querySelector('.tecla_splash').onclick = playSongSplash;
-document.querySelector('.tecla_toim').onclick = playSongToim;
-document.querySelector('.tecla_psh').onclick = playSongPsh;
-document.querySelector('.tecla_tic').onclick = playSongTic;
-document.querySelector('.tecla_tom').onclick = playSongTom;
+listOfKeys[0].onclick = playSongPom;
+
+
 
 
