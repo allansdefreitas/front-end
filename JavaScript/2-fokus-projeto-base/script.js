0e2105332f20fa2cd61bdfe48507df9b55cbf962
allansdefreitas <allan.allan.santos@gmail.com>
2024-05-20 12:48:41 -0300

    DOM

--- JavaScript/2-fokus-projeto-base/script.js
+++ JavaScript/2-fokus-projeto-base/script.js
@@ -6,30 +6,43 @@ const descansoCurtoButton = document.querySelector('.app__card-button--curto');
 const descansoLongoButton = document.querySelector('.app__card-button--longo');
 const appImage = document.querySelector('.app__image');
 
-const timerButton = document.querySelector('.app__card-primary-button');
+const timerButton = document.querySelector('#start-pause');
+const timerButtonText = document.querySelector('#start-pause span');
+
 const timer = document.querySelector('.app__card-timer'); // or '#timer'
 const appTitle = document.querySelector('.app__title');
 const appTitleStrong = document.querySelector('.app__title-strong');
 
 const toggleCheckBoxMusic = document.querySelector('#alternar-musica');
-const song = new Audio('/sons/luna-rise-part-one.mp3');
-song.loop = true;
+
+const songFoco = new Audio('/sons/luna-rise-part-one.mp3');
+const songPlay = new Audio('/sons/play.wav');
+const songPause = new Audio('/sons/pause.mp3');
+const songEndTimer = new Audio('/sons/beep.mp3');
+
+songPlay.volume = 0.3;
+songPause.volume = 0.3;
+songEndTimer.volume = 0.4;
+
+songFoco.loop = true;
 
 toggleCheckBoxMusic.addEventListener('change', ()=>{
     // console.log(song.currentTime);
     // console.log(song.volume);
     
-    if(song.paused){
-        song.play();
+    if(songFoco.paused){
+        songFoco.play();
     }else{
-        song.pause();
+        songFoco.pause();
     }
 });
-
 
 const timeFoco = 1500;
 const timeDescansoCurto = 300;
 const timeDescansoLongo = 900;
+
+let elapsedTimeInSeconds = 5;
+let intervalId = null;
 
 focoButton.addEventListener('click', ()=> {
     changeContext('foco');
@@ -60,6 +73,7 @@ function changeContext(context){
             Otimize sua produtividade,<br>
                 <strong class="app__title-strong">mergulhe no que importa.</strong>`;
             focoButton.classList.add("active");
+            // elapsedTimeInSeconds = timeFoco;
             break;
 
         case "descanso-curto":
@@ -67,16 +81,54 @@ function changeContext(context){
                 <strong class="app__title-strong">Faça uma pausa curta!</strong>`;
 
             descansoCurtoButton.classList.add("active");
+            // elapsedTimeInSeconds = timeDescansoCurto;
             break;
         case "descanso-longo":
             appTitle.innerHTML = `Hora de voltar à superfície.<br>
                 <strong class="app__title-strong">Faça uma pausa longa.</strong>`;
             descansoLongoButton.classList.add("active");
+            // elapsedTimeInSeconds = timeDescansoLongo;
             break;
     
         default:
             break;
     }
+}
+
+const countDown = () => {
+    
+    elapsedTimeInSeconds -= 1;
+    console.log('Timer: ' + elapsedTimeInSeconds);
+    
+    if(elapsedTimeInSeconds <= 0){
+        resetCountDown();
+        alert("Finished time!");
+        songEndTimer.play();
+        return
+    }
+}
+
+timerButton.addEventListener('click', startOrPauseCountDown);
+
+function startOrPauseCountDown(){ 
+    
+    if(intervalId){ // If intervalId has a value (is not null)
+        resetCountDown();
+        
+        songPause.play();
+        return
+    }else{
+        songPlay.play();
+        timerButtonText.textContent = "Pausar";
+    }     
+
+    intervalId = setInterval(countDown, 1000); // 1000 miliseconds = 1 second
+}
+
+function resetCountDown(){
+    timerButtonText.textContent = "Começar";
+    clearInterval(intervalId); 
+    intervalId = null;
 }
 
 /*
