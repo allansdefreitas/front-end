e5e94ac2b9d3fc0d3e6e9d6991e15d129bbd5675
allansdefreitas <allan.allan.santos@gmail.com>
2024-05-23 12:27:04 -0300

    TypeScript

--- TypeScript/byte-bank/js/bytebank.js
+++ TypeScript/byte-bank/js/bytebank.js
@@ -1,44 +1,38 @@
-let saldo = 3000;
-
-const elementoSaldo = document.querySelector(".saldo-valor .valor");
-elementoSaldo.textContent = saldo;
-
-const elementoFormulario = document.querySelector(".block-nova-transacao form");
-
+var saldo = 3000;
+var elementoSaldo = document.querySelector(".saldo-valor .valor"); // Assures that this input be an HTML Element
+if (elementoSaldo != null) {
+    elementoSaldo.textContent = saldo.toString();
+}
+var elementoFormulario = document.querySelector(".block-nova-transacao form"); // I know that it is an HTML form
 elementoFormulario.addEventListener("submit", function (event) {
     event.preventDefault(); // Submit form without reload page (to avoid the lost of data)
     if (elementoFormulario.checkValidity() == false) {
         alert("Please, fill all fields of transaction!");
         return;
     }
-
-    const inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao");
-    const inputValor = elementoFormulario.querySelector("#valor");
-    const inputData = elementoFormulario.querySelector("#data");
-
-    let tipoTransacao = inputTipoTransacao.value;
-    let valor = inputValor.value;
-    let data = inputData.value;
-
+    var inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao");
+    var inputValor = elementoFormulario.querySelector("#valor");
+    var inputData = elementoFormulario.querySelector("#data");
+    var tipoTransacao = inputTipoTransacao.value;
+    var valor = inputValor.valueAsNumber;
+    var data = new Date(inputData.value);
+    //     console.log(typeof(valor));
     if (tipoTransacao == "Depósito") {
-            saldo += valor;
-    } else if (tipoTransacao == "Transferência" || tipoTransacao == "Pagamento de Boleto") {
-            saldo -= valor;
-    }else{
-            alert("Tipo de transação inválido!");
+        saldo += valor;
     }
-
+    else if (tipoTransacao == "Transferência" || tipoTransacao == "Pagamento de Boleto") {
+        saldo -= valor;
+    }
+    else {
+        alert("Tipo de transação inválido!");
+    }
     console.log("saldo: " + saldo);
-    elementoSaldo.textContent = saldo;
-
-    const novaTransacao = {
+    elementoSaldo.textContent = saldo.toString();
+    var novaTransacao = {
         tipoTransacao: tipoTransacao,
         valor: valor,
         data: data,
     };
-
-  console.log(novaTransacao);
-
-  elementoFormulario.reset(); // Clean all fields
-
+    console.log(novaTransacao);
+    elementoFormulario.reset(); // Clean all fields
 });
