4047349219e7a8335ccbc941b4371932e3b96119
allansdefreitas <allan.allan.santos@gmail.com>
2024-05-31 11:37:15 -0300

    Angular

--- Angular/indexa/src/app/app.component.ts
+++ Angular/indexa/src/app/app.component.ts
@@ -4,6 +4,16 @@ import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './componentes/container/container.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { SeparadorComponent } from './componentes/separador/separador.component';
+import { ContatoComponent } from './componentes/contato/contato.component';
+
+
+interface Contato {
+  id: number
+  nome: string
+  telefone: string
+}
+
+import contatosFromJSON from './agenda.json';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +23,8 @@ import { SeparadorComponent } from './componentes/separador/separador.component'
     RouterOutlet, 
     ContainerComponent, 
     CabecalhoComponent, 
-    SeparadorComponent
+    SeparadorComponent,
+    ContatoComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
@@ -21,7 +32,18 @@ import { SeparadorComponent } from './componentes/separador/separador.component'
 export class AppComponent {
 
   alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
+  contatos: Contato[] = contatosFromJSON;
 
+  filtrarContatosPorLetraInicial(letra: string): Contato[]{
+
+    return this.contatos.filter ( contato => {
+      let letraLowerCase: string = contato.nome.toLowerCase();
+      return letraLowerCase.startsWith(letra);
+    })    
+
+    return this.contatos;
+
+  }
   
 
 }
