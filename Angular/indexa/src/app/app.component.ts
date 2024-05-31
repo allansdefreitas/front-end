a9ad999e2dba73cbb1cc8d017ba53c996ed9971a
allansdefreitas <allan.allan.santos@gmail.com>
2024-05-31 14:31:40 -0300

    Angular

--- Angular/indexa/src/app/app.component.ts
+++ Angular/indexa/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { ContainerComponent } from './componentes/container/container.component'
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { SeparadorComponent } from './componentes/separador/separador.component';
 import { ContatoComponent } from './componentes/contato/contato.component';
+import { FormsModule } from '@angular/forms';
 
 
 interface Contato {
@@ -24,7 +25,8 @@ import contatosFromJSON from './agenda.json';
     ContainerComponent, 
     CabecalhoComponent, 
     SeparadorComponent,
-    ContatoComponent
+    ContatoComponent,
+    FormsModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
@@ -34,11 +36,42 @@ export class AppComponent {
   alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
   contatos: Contato[] = contatosFromJSON;
 
+  textoBusca: string = '';
+
+
+  getTextoBusca(){
+    return this.textoBusca;
+  }
+
+  filtrarContatosPorTexto(): Contato[]{
+
+    // this.text
+
+    // or if(!this.textoBusca)
+    if(this.textoBusca.length ===0){
+      return this.contatos;
+    }else{
+      return this.contatos.filter(contato => {
+        let ithContatoNome = contato.nome;
+        let ithContatoNomeLower: string = ithContatoNome.toLowerCase();
+
+        return ithContatoNomeLower.includes(this.textoBusca);
+
+      })
+    }
+
+
+
+  }
+
   filtrarContatosPorLetraInicial(letra: string): Contato[]{
 
-    return this.contatos.filter ( contato => {
-      let letraLowerCase: string = contato.nome.toLowerCase();
-      return letraLowerCase.startsWith(letra);
+    let contatosPorTextoReturned: Contato[] = this.filtrarContatosPorTexto();
+    
+    // or return this.filtrarContatosPorTexto.filter(...
+    return this.filtrarContatosPorTexto().filter ( contato => {
+      let letraInicialNome: string = contato.nome.toLowerCase();
+      return letraInicialNome.startsWith(letra);
     })    
 
     return this.contatos;
