4022182fc7afba31849e7d86f62ad6e1a93dfbf6
Allan Freitas <allan.freitas@serpro.gov.br>
2024-07-19 13:18:37 -0300

    JS: Acessibility - tablist navigation and preventDefault events

--- JavaScript/vidflow-js-acessibility/js/botoesCategorias.js
+++ JavaScript/vidflow-js-acessibility/js/botoesCategorias.js
@@ -8,11 +8,14 @@ botoesCategorias.forEach((botao) => {
     filtrarPorCategoria(categoriaSelecionada);
     atualizarEstadosDosBotoes(categoriaSelecionada);
   });
+
+  botao.addEventListener("keydown", changeFocusByKeyboard)
 });
 
 function associarPainel(categoriaSelecionada){
+  console.log(categoriaSelecionada);
   const painelVideos = document.querySelector('[role="tabpanel"]');
-  const idButton = document.querySelector(`[name="${categoriaSelecionada}]`).id;
+  const idButton = document.querySelector(`[name="${categoriaSelecionada}"]`).id;
 
   painelVideos.setAttribute("aria-labelledby", idButton);
 }
@@ -35,9 +38,59 @@ function atualizarEstadosDosBotoes(categoriaSelecionada) {
     const botaoFoiSelecionado = botao.getAttribute("name") === categoriaSelecionada;
 
     botao.ariaSelected = botaoFoiSelecionado;
+    let tabindexValue = 0;
+    if(botaoFoiSelecionado === true){
+      tabindexValue = 0;
+    } else{
+      tabindexValue = -1;
+    }
+    console.log(tabindexValue);
+    botao.setAttribute("tabindex", tabindexValue);
     
     // botao.classList.toggle("selecionado", botaoFoiSelecionado);
   })
 }
 
+const tablist = document.querySelector('[role="tablist"]');
 
+function changeFocusByKeyboard(event){
+
+  const currentButton = event.target;
+  let nextButton;
+  
+  event.preventDefault(); // Avoid that Arrows and Home or End move the tablist horizontally (Arrows) or the screen vertically (Home and End)
+  
+  if(event.key === "ArrowRight"){
+    if(currentButton === tablist.lastElementChild){
+       tablist.firstElementChild.focus();
+    }
+
+    nextButton = currentButton.nextElementSibling;
+
+    nextButton.focus();
+
+    // currentButton.setAttribute("tabindex", "-1");
+    // nextButton.setAttribute("tabindex", "0");
+
+  }else if(event.key === "ArrowLeft"){
+
+    if(currentButton === tablist.firstElementChild){
+      tablist.lastElementChild.focus();
+    }else{
+      nextButton = currentButton.previousElementSibling;
+      nextButton.focus();
+
+      // currentButton.setAttribute("tabindex", "-1");
+      // nextButton.setAttribute("tabindex", "0");
+    }
+  }else if(event.key === "Home"){
+    tablist.firstElementChild.focus();
+  }else if(event.key === "End"){
+    tablist.lastElementChild.focus();
+  }
+
+  nextButton.scrollIntoView({
+    behavior: "smooth"
+  });
+
+}
