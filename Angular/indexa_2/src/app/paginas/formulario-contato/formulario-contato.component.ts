d5d357af402e6688300ad430803630160499e4b7
Allan Freitas <allan.freitas@serpro.gov.br>
2024-09-07 20:17:19 -0300

    Angular: Reactive (or data-driven forms)

--- Angular/indexa_2/src/app/paginas/formulario-contato/formulario-contato.component.ts
+++ Angular/indexa_2/src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from "../../componentes/container/container.component";
 import { SeparadorComponent } from "../../componentes/separador/separador.component";
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-contato',
@@ -9,10 +10,26 @@ import { SeparadorComponent } from "../../componentes/separador/separador.compon
     ContainerComponent,
     ContainerComponent,
     SeparadorComponent,
+    ReactiveFormsModule
 ],
   templateUrl: './formulario-contato.component.html',
   styleUrl: './formulario-contato.component.css'
 })
 export class FormularioContatoComponent {
+  
+  contatoForm!: FormGroup;
+
+  constructor(){
+    this.contatoForm = new FormGroup({
+      nome: new FormControl('Pedro'),
+      telefone: new FormControl(''),
+      email: new FormControl(''),
+      aniversario: new FormControl(''),
+      redes: new FormControl(''),
+      observacoes: new FormControl(''),
+
+    });
+  }
+
 
 }
